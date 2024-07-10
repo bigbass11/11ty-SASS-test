8e3064513507f7b3785bfa4db0504958b328bb23
bigbass11 <92268911+bigbass11@users.noreply.github.com>
2024-07-09 20:50:52 -0600

    done crash course 2 by Jaydan Urwin

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,19 @@
 module.exports = function(eleventyConfig) {
-    return {
-      dir: {
-        input: 'src',
-        includes: '_includes',
-        output: '_site',
-      },
-      templateFormats: ['md', 'njk', 'html'],
-      markdownTemplateEngine: 'njk',
-      htmlTemplateEngine: 'njk',
-      dataTemplateEngine: 'njk',
-    };
-  }
+
+  eleventyConfig.addPassthroughCopy("src/assets/");
+  eleventyConfig.addPassthroughCopy("src/css/");
+
+  eleventyConfig.addWatchTarget("./src/css/");
+  
+  return {
+    dir: {
+      input: 'src',
+      includes: '_includes',
+      output: '_site',
+    },
+    templateFormats: ['md', 'njk', 'html'],
+    markdownTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk',
+    dataTemplateEngine: 'njk',
+  };
+}
